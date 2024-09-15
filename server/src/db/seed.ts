6242c18f25779d9a230384d3d0c85167a379b76c
neIsonp <nelsonponte37@gmail.com>
2024-09-15 11:22:02 +0000

    setup backend

---
+++ server/src/db/seed.ts
@@ -0,0 +1,28 @@
+import dayjs from "dayjs";
+import { client, db } from ".";
+import { goalCompletions, goals } from "./schema";
+
+async function seed() {
+  await db.delete(goalCompletions);
+  await db.delete(goals);
+
+  const result = await db
+    .insert(goals)
+    .values([
+      { title: "Acordar às 5 am", desiredWeeklyFrequency: 7 },
+      { title: "Ler 10 págnas", desiredWeeklyFrequency: 7 },
+      { title: "Estudar", desiredWeeklyFrequency: 7 },
+    ])
+    .returning();
+
+  const startOfWeek = dayjs().startOf("week");
+
+  await db.insert(goalCompletions).values([
+    { goalId: result[0].id, createdAt: startOfWeek.toDate() },
+    { goalId: result[1].id, createdAt: startOfWeek.add(1, "day").toDate() },
+  ]);
+}
+
+seed().finally(() => {
+  client.end();
+});
